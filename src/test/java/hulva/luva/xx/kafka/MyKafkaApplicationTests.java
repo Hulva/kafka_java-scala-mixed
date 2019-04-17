4b87f0b81aaa5be01f1a4cc87c42b41a2c424228
Hulva Luva.H <Luva.L.Hua@newegg.com>
2019-04-17 16:52:29 +0800

    :tada: :sparkles: :heavy_plus_sign: runnable

---
+++ src/test/java/hulva/luva/xx/kafka/MyKafkaApplicationTests.java
@@ -0,0 +1,16 @@
+package hulva.luva.xx.kafka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyKafkaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
